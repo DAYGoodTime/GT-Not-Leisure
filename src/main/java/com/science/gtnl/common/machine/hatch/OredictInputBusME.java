2676ed6543a3837bdb2903c5885cb3a6a5dca9e6
DAYGood_Time <35392263+DAYGoodTime@users.noreply.github.com>
2026-07-13 15:00:29 +0800

    Update OredictInputBusME.java
    spotless apply

--- src/main/java/com/science/gtnl/common/machine/hatch/OredictInputBusME.java
+++ src/main/java/com/science/gtnl/common/machine/hatch/OredictInputBusME.java
@@ -129,10 +129,12 @@ public class OredictInputBusME extends MTEHatchInputBusME implements IRecipeProc
             disableSort = true;
         }
     }
+
     @Override
     public MetaTileEntity newMetaEntity(IGregTechTileEntity aTileEntity) {
         return new OredictInputBusME(mName, autoPullAvailable, mTier, mDescriptionArray, mTextures, isSuper);
     }
+
     @Override
     public int getSizeInventory() {
         return SIDE_SLOT_COUNT;
